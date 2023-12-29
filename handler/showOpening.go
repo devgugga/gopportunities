d6322dd2647a91562f73b298ee219942d86db2c4
Gustavo Gomes <120190910+devgugga@users.noreply.github.com>
2023-12-29 07:43:40 -0300

    ✨ Add functionality to show specific opening by ID

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -3,11 +3,24 @@ package handler
 import (
 	"net/http"
 
+	"github.com/devgugga/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"Show": "Openings",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "Param id (string) is required").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
